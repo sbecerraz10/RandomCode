79fa5fc5f4fbb89619a6f880bd3ae65f3a32e26d
Sebastian Becerra Z <43484683+sbecerraz10@users.noreply.github.com>
2023-05-02 15:53:36 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,25 @@
-import markdown
+import os
+import mistune
 from docx import Document
-from docx.shared import Inches
 
-def convert_md_to_docx(input_file_path, output_file_path):
-    # Read the markdown file
-    with open(input_file_path, 'r') as input_file:
-        text = input_file.read()
 
-    # Convert markdown to HTML
-    html = markdown.markdown(text)
+def markdown_to_word(input_file_path, output_file_path):
+    # Load the Markdown content from the input file
+    with open(input_file_path, 'r') as f:
+        markdown_content = f.read()
+
+    # Parse the Markdown content to HTML
+    html_content = mistune.html(markdown_content)
 
     # Create a new Word document
-    doc = Document()
+    document = Document()
 
-    # Add the converted HTML to the document
-    doc.add_paragraph(html)
+    # Add the paragraphs from the HTML content to the Word document
+    for paragraph in html_content.split('<p>'):
+        if paragraph.strip():
+            p = document.add_paragraph(paragraph.replace('</p>', ''))
 
-    # Save the document as a .docx file
-    doc.save(output_file_path + '.docx')
+    # Save the Word document
+    document.save(os.path.splitext(output_file_path)[0] + '.docx')
+
 
